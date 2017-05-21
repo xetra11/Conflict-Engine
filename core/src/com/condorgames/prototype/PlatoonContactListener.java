ee8f5796fca2c103ea5143bff5868b8a48dc1f97
Patrick Hoefer <p.hoefer@babiel.com>
2017-05-21 21:18:00 +0200

    first sound event tryout

--- core/src/com/condorgames/prototype/PlatoonContactListener.java
+++ core/src/com/condorgames/prototype/PlatoonContactListener.java
@@ -1,5 +1,8 @@
 package com.condorgames.prototype;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
@@ -10,9 +13,14 @@ public class PlatoonContactListener implements ContactListener {
   @Override
   public void beginContact(Contact contact) {
     SteerablePlatoonEntity platoon = (SteerablePlatoonEntity) contact.getFixtureA().getBody().getUserData();
-    if(platoon.getPlatoonID() == 1){
+    if (platoon.getPlatoonID() == 1) {
       platoon.setTagged(true);
-      platoon.getBody().setLinearVelocity(0f,0f);
+      platoon.getBody().setLinearVelocity(0f, 0f);
+      Music background = Gdx.audio.newMusic(Gdx.files.internal("combat.wav"));
+      Music ammoReport = Gdx.audio.newMusic(Gdx.files.internal("chatter_ammo.wav"));
+      background.play();
+      ammoReport.play();
+      ammoReport.setOnCompletionListener(Music -> background.stop());
     }
   }
 
