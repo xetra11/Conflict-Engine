339ecd97c34fd7245ad9dfe3a1c6e9361dcf1653
Patrick Hoefer <p.hoefer@babiel.com>
2017-05-26 22:58:04 +0200

    SoldierProperty test added

---
+++ core/src/test/java/com/condorgames/prototype/entities/SoldierPropertiesImplTest.java
@@ -0,0 +1,76 @@
+package com.condorgames.prototype.entities;
+
+import com.condorgames.prototype.battleresolver.Morale;
+import com.condorgames.prototype.battleresolver.Morale.MoraleState;
+import com.condorgames.prototype.entities.SoldierProperties.Health;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SoldierPropertiesImplTest {
+
+  private SoldierPropertiesImpl soldierProperties;
+
+  @Before
+  public void setup() {
+    soldierProperties = new SoldierPropertiesImpl(MoraleState.NORMAL, Health.OK);
+  }
+
+  @Test
+  public void shouldDecreaseMoraleOneLower() throws Exception {
+    MoraleState expected = MoraleState.LOW;
+    soldierProperties.decreaseMorale();
+    MoraleState actual = soldierProperties.getMorale();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void shouldRaiseMoraleOneHigher() throws Exception {
+    MoraleState expected = MoraleState.HIGH;
+    soldierProperties.raiseMorale();
+    MoraleState actual = soldierProperties.getMorale();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void shouldNotLowerBeyondLast() {
+    MoraleState expected = MoraleState.PINNED_DOWN;
+    soldierProperties.decreaseMorale();
+    soldierProperties.decreaseMorale();
+    soldierProperties.decreaseMorale();
+    soldierProperties.decreaseMorale();
+    soldierProperties.decreaseMorale();
+    soldierProperties.decreaseMorale();
+    soldierProperties.decreaseMorale();
+    MoraleState actual = soldierProperties.getMorale();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void shouldNotRaiseBeyondFirst() {
+    MoraleState expected = MoraleState.FANATIC;
+    soldierProperties.raiseMorale();
+    soldierProperties.raiseMorale();
+    soldierProperties.raiseMorale();
+    soldierProperties.raiseMorale();
+    soldierProperties.raiseMorale();
+    soldierProperties.raiseMorale();
+    soldierProperties.raiseMorale();
+    MoraleState actual = soldierProperties.getMorale();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void shouldReturnValueOfMorale() {
+    int expected = 3;
+    int actual = soldierProperties.getMorale().getValue();
+    assertEquals(expected, actual);
+    soldierProperties.raiseMorale();
+    expected = 4;
+    actual = soldierProperties.getMorale().getValue();
+    assertEquals(expected, actual);
+  }
+
+
+}
