c0250aed4ab653571da8f7547ff7b1f913893e13
Patrick Hoefer <p.hoefer@babiel.com>
2017-05-31 20:51:07 +0200

    cooldown added ticking functionality

--- core/src/com/condorgames/prototype/helper/Cooldown.java
+++ core/src/com/condorgames/prototype/helper/Cooldown.java
@@ -3,18 +3,30 @@ package com.condorgames.prototype.helper;
 
 public class Cooldown {
 
+  private final boolean ticking;
   float timeToWaitFor;
   float resetTime;
 
   public Cooldown(float timeToWaitFor){
     this.timeToWaitFor = timeToWaitFor;
     this.resetTime = timeToWaitFor;
+    this.ticking = false;
   }
+
+  public Cooldown(float timeToWaitFor, boolean ticking){
+    this.timeToWaitFor = timeToWaitFor;
+    this.resetTime = timeToWaitFor;
+    this.ticking = ticking;
+  }
+
 
   public boolean isDone(float deltaTime, CooldownFinishedListener cooldownFinishedListener){
     timeToWaitFor -= deltaTime;
     if(timeToWaitFor <= 0){
       cooldownFinishedListener.onFinish();
+      if (ticking) {
+        reset();
+      }
       return true;
     }
     return false;
